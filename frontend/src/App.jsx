c683e4d55f964ac3a88ed4b8d336d742a35fc7d0
nicolajack <nicolajack@gmail.com>
2025-04-10 12:23:02 -0400

    added files from working repo to forked repo

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+// import statements for app componenets and css
+import Background from './components/background/background';
+import Logo from './components/logo/logo';
+import './App.css';
+import darkToggle from './assets/darktoggle.png';
+
+function App() {
+    function darkMode() {
+        const element = document.body;
+        element.classList.toggle("dark-mode");
+    }
+    return (
+        <>
+            <Background />
+            <button id="darkMode"onClick={darkMode}><img width="30px" height="30px" src={darkToggle}/></button>
+            <Logo />
+            <h1 id="info">simply drag the pin to any location you’d like, and <br /> 
+            click to see the sunrise and sunset time for that area!!</h1>
+        </>
+    )
+}
+
+export default App
